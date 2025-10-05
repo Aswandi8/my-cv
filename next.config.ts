a3911c8595a400efd9c27c9219e4a30060cd1de8
Aswandi8 <aswandibaso08@gmail.com>
2025-10-05 22:26:53 +0700

    Cek Image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  basePath: "/my-cv",
+  assetPrefix: "/my-cv/",
+  images: {
+    unoptimized: true, // ⚠️ ini disable Image Optimization
+  },
 };
 
 export default nextConfig;
